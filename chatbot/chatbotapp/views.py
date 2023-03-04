f1e0d2b18a46ad45f0d8855488fc65a3f3fcea0d
saif955 <99525405+saif955@users.noreply.github.com>
2023-03-04 19:16:20 +0600

    ok

--- chatbot/chatbotapp/views.py
+++ chatbot/chatbotapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpRequest
-
+import random
 
 def homepage(request):
-    return render(request, 'temp1.html')
+    return render(request, 'temp1.html', {'number': random.randint(1, 50)})
