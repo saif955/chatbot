68bee84627825f6f202a2d30bd4fb9c4f63b1d86
saif955 <99525405+saif955@users.noreply.github.com>
2023-03-04 13:41:22 +0600

    slight change

--- chatbot/chatbotapp/views.py
+++ chatbot/chatbotapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpRequest
 
-# Create your views here.
+
+def homepage(request):
+    return HttpResponse('Hello World')
