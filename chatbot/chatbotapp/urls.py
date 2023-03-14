14249c0a17d2447c717a39b87475a2b7009e6d05
saif955 <99525405+saif955@users.noreply.github.com>
2023-03-14 23:32:58 +0600

    tuesday_last

--- chatbot/chatbotapp/urls.py
+++ chatbot/chatbotapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='upload'),
-    
+    path('post/', views.post, name='post')
   
 ]
--- chatbot/chatbotapp/views.py
+++ chatbot/chatbotapp/views.py
@@ -4,21 +4,35 @@ from chatbotapp.forms import UserInputForm
 
 from .models import*
 
-
+# this directs to the homepage of chatbot
 def homepage(request):
-    text= ' '
     posts = UserInput.objects.all()
-    form = UserInputForm(request.POST)
-    if form.is_valid():
-        form.save()
-        text = form.cleaned_data[ 'input' ]
-        for post in posts:
-            if not post.output:
-                post.output= text
-                post.save()
-            else:
-                continue
-        posts = UserInput.objects.all()
-        return redirect('upload')
-    args = {'text': text, 'form': form, 'posts': posts}
+    args = { 'posts': posts}
     return render(request, 'temp1.html', args)
+
+
+# this takes in data from user and gives out chatbot's output 
+def post(request):
+    if request.method == 'POST':
+        text= ' '
+        posts = UserInput.objects.all()
+        form = UserInputForm(request.POST)
+        if form.is_valid():
+            form.save()
+            text = form.cleaned_data[ 'input' ]
+            for post in posts:
+                if not post.output:
+                    post.output= text
+                    post.save()
+                else:
+                    continue
+            return redirect('upload')
+        else:
+            form= UserInputForm()
+
+            
+    return redirect('upload')
+
+
+    
+    
