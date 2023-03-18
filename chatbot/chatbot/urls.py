9fe0a6b59cbe5e9a7dada0c7dcc9b2bf43bda21a
saif955 <99525405+saif955@users.noreply.github.com>
2023-03-18 14:15:13 +0600

    django version

--- chatbot/chatbot/urls.py
+++ chatbot/chatbot/urls.py
@@ -15,10 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views.static import serve
-from django.conf.urls import url
-from django.conf.urls.static import static
-from django.conf import settings
 
 
 urlpatterns = [
@@ -26,8 +22,6 @@ urlpatterns = [
     path('', include('chatbotapp.urls')),
     path('accounts/', include('allauth.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    
 ]
 
-urlpatterns= urlpatterns+static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
